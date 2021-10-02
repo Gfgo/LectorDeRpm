57719c613ac8345fcefe4d77e54e449677595a92
Gfgo <gfgo033@hotmail.com>
2021-10-02 13:41:57 -0500

    Update LectorDeRpm222.ino

--- LectorDeRpm222/LectorDeRpm222.ino
+++ LectorDeRpm222/LectorDeRpm222.ino
@@ -3,7 +3,7 @@
 int seg=0;
 volatile int  cont = 0;
 
-void inter(){
+void inter(){                     // Funcion que se ejecuta durante cada interrupion
   cont ++;
   
   }
@@ -19,7 +19,7 @@ void loop() {
   Serial.println(seg);
   seg++;
   delay(999);
-  cont*=7.5;                 // Como son 8 interrupciones por vuelta (contador * (60/8)=7.5)
+  cont*=7.5;                      // Como son 8 interrupciones por vuelta (contador * (60/8)=7.5)
   Serial.print(cont);             //  El numero 8 depende del numero aspas de la helise del motor en prueba
   Serial.println(" interrup");
   cont=0;
