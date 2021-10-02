fde7d2c328a3ad816dd0a59cdbfdae36d1088b26
Gfgo <gfgo033@hotmail.com>
2021-10-02 13:38:52 -0500

    rpm222 lee rpm, no mapea falta bluetooth

---
+++ LectorDeRpm222/LectorDeRpm222.ino
@@ -0,0 +1,26 @@
+#define vel 2
+
+int seg=0;
+volatile int  cont = 0;
+
+void inter(){
+  cont ++;
+  
+  }
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(vel, INPUT);
+  attachInterrupt(digitalPinToInterrupt(vel), inter, RISING);
+}
+
+void loop() {
+  Serial.println(seg);
+  seg++;
+  delay(999);
+  cont*=7.5;                 // Como son 8 interrupciones por vuelta (contador * (60/8)=7.5)
+  Serial.print(cont);             //  El numero 8 depende del numero aspas de la helise del motor en prueba
+  Serial.println(" interrup");
+  cont=0;
+}
--- LectorDeRpmESP/LectorDeRpmESP.ino
+++ LectorDeRpmESP/LectorDeRpmESP.ino
@@ -1,23 +1,42 @@
-#define       vel 27
-int           cont=0;
+#define       vel 2
+
+volatile float  contador = 0;   // Variable entera que se almacena en la RAM del Micro
+int           velpot =0;
 int           senalto=1023;
 int           senbajo=0;
-
-
+ 
 void setup() {
   Serial.begin(115200);
-  pinMode(vel, INPUT);
-  attachInterrupt(vel, inerrupcion, RISING);
-}
+  attachInterrupt(digitalPinToInterrupt(vel), inter, RISING);  // Interrupcion 0 (pin2) LOW, CHANGE, RISING, FALLING
+  while (millis()<5000){
+    velpot = analogRead (vel);
+    if (velpot<senalto){
+      senalto=velpot;
+    }
+     if (velpot>senbajo){
+      senbajo=velpot;   
+   }
 
+ }
+
+}         
+ 
 void loop() {
-  Serial.println(cont);
- // Serial.println(vel);
-  cont++;
-  delay(200);
-}
+  velpot=analogRead(vel);
+  int pitch = map(velpot,senbajo, senalto, 80, 1000);
 
-void inerrupcion (){
-  Serial.println ("Interrupción");
-  Serial.println(vel);
+  
+  delay(999);               // retardo de casi 1 segundo
+  contador *=7.5;
+  Serial.print(contador); // Como son 8 interrupciones por vuelta (contador * (60/8)=7.5)
+  Serial.println(" RPM1");    //  El numero 8 depende del numero aspas de la helise del motor en prueba
+  //Serial.println(velpot);
+  //contador = 0;
+}
+ 
+void inter()    // Funcion que se ejecuta durante cada interrupion
+{
+   contador++;   // Se incrementa en uno el contador
+   Serial.print(contador); 
+   Serial.println(" RPM2");
 }
